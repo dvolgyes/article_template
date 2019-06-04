fe56425e6a59c6fdb7468c81a8339d5cf2d9d1eb
David Völgyes <david.volgyes@ieee.org>
2019-06-04 18:44:58 +0200

    code reformatting

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,48 +5,53 @@ import os
 from pathlib import Path
 from collections import defaultdict
 
-def wget_as(url,filename, save_url = False):
+
+def wget_as(url, filename, save_url=False):
     tempfile = Path(wget.download(url))
     if tempfile.exists():
         tempfile.rename(filename)
     else:
         sys.exit(1)
     if save_url:
-        with open(filename+".url", "wt") as f:
+        with open(filename + ".url", "wt") as f:
             f.write(url)
             f.write("\n")
 
 
-if '{{cookiecutter.online_gitignore}}'.lower() == 'y':
-    wget_as('https://github.com/github/gitignore/blob/master/TeX.gitignore','.gitignore')
+if "{{cookiecutter.online_gitignore}}".lower() == "y":
+    wget_as(
+        "https://github.com/github/gitignore/blob/master/TeX.gitignore", ".gitignore"
+    )
 
-#templates:
-templates={
-"arxiv": "https://raw.githubusercontent.com/kourgeorge/arxiv-style/master/arxiv.sty",
-"nips_2018": "https://media.nips.cc/Conferences/NIPS2018/Styles/nips_2018.sty"
+# templates:
+templates = {
+    "arxiv": "https://raw.githubusercontent.com/kourgeorge/arxiv-style/master/arxiv.sty",
+    "nips_2018": "https://media.nips.cc/Conferences/NIPS2018/Styles/nips_2018.sty",
 }
 
-style = '{{cookiecutter.style}}'
+style = "{{cookiecutter.style}}"
 if style in templates:
     filename = wget.download(templates[style])
-    with open(filename+".url", "wt") as f:
+    with open(filename + ".url", "wt") as f:
         f.write(templates[style])
         f.write("\n")
 
-os.system('git init .')
-os.system('git add .')
+os.system("git init .")
+os.system("git add .")
 os.system('git commit . -m "Initial commit."')
-git_backend = '{{cookiecutter.git_backend}}'.lower()
-if git_backend != 'none':
+git_backend = "{{cookiecutter.git_backend}}".lower()
+if git_backend != "none":
     git_server = None
-    if git_backend == 'github':
-        git_server = 'github.com'
-    elif git_backend == 'bitbucket':
-        git_server = 'bitbucket.org'
-    elif git_backend == 'gitlab':
-        git_server = 'gitlab.com'
-    elif git_backend == 'uio':
-        git_server = 'github.uio.no'
+    if git_backend == "github":
+        git_server = "github.com"
+    elif git_backend == "bitbucket":
+        git_server = "bitbucket.org"
+    elif git_backend == "gitlab":
+        git_server = "gitlab.com"
+    elif git_backend == "uio":
+        git_server = "github.uio.no"
     else:
         git_server = git_backend
-    os.system('git remote add origin git@{git_server}:{{cookiecutter.username}}/{{cookiecutter.project_slug}}')
+    os.system(
+        "git remote add origin git@{git_server}:{{cookiecutter.username}}/{{cookiecutter.project_slug}}"
+    )
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from pip._internal import main as pipmain
 
-packages = ('wget',)
+packages = ("wget",)
 for package in packages:
-    pipmain(['install',package])
+    pipmain(["install", package])
